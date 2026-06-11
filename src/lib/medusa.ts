7fb37ede1d877a14b49797ed3a7bcdd9bb7ee612
anotherbondan <anandagautama3112@gmail.com>
2026-06-11 10:21:22 +0700

    chore: disable medusa client until publishable key is configured

--- src/lib/medusa.ts
+++ src/lib/medusa.ts
@@ -1,10 +1,13 @@
-import Medusa from "@medusajs/js-sdk";
+// TODO: set NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY to re-enable
+// import Medusa from "@medusajs/js-sdk";
+//
+// const baseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || "http://localhost:9000";
+// const publishableKey = process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || "";
+//
+// export const medusa = new Medusa({
+//   baseUrl,
+//   publishableKey,
+//   debug: process.env.NODE_ENV === "development",
+// });
 
-const baseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || "http://localhost:9000";
-const publishableKey = process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || "";
-
-export const medusa = new Medusa({
-  baseUrl,
-  publishableKey,
-  debug: process.env.NODE_ENV === "development",
-});
+export const medusa = null;
