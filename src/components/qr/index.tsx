950d96401eba845a6930274288c818ac0ebee12e
anotherbondan <anandagautama3112@gmail.com>
2026-06-30 21:59:15 +0700

    feat: add qr engine

---
+++ src/components/qr/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/**
+ * QR Code Component
+ */
+export function QRCodeComponent() {
+  return (
+    <div>
+      {/* TODO: Implement QR Code UI */}
+    </div>
+  );
+}
