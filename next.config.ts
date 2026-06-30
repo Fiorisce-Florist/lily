e90a539f7265df3743c9aa187c0e379d11fecead
anotherbondan <anandagautama3112@gmail.com>
2026-07-01 01:45:36 +0700

    chore: add cdn url to next config

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "www.google.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
