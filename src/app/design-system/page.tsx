c7504ae3e6fea286fbc90afe45646e187fddc70e
anotherbondan <anandagautama3112@gmail.com>
2026-07-06 18:24:37 +0700

    feat: configure design system page can only be accesed on development

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -1,5 +1,9 @@
 import DesignSystemModule from "@/modules/DesignSystemModule";
+import { notFound } from "next/navigation";
 
 export default function DesignSystemPage() {
+  if (process.env.NODE_ENV !== "development") {
+    notFound();
+  }
   return <DesignSystemModule />;
 }
